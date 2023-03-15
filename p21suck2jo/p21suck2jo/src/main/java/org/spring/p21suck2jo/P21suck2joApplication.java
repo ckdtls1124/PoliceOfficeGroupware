b2117255984c152a10116b9845dfb5bd2748ca81
ckdtls1124 <ckdtls1124@naver.com>
2023-03-15 17:40:22 +0900

    0315_DefaultProjectFile'

---
+++ p21suck2jo/p21suck2jo/src/main/java/org/spring/p21suck2jo/P21suck2joApplication.java
@@ -0,0 +1,13 @@
+package org.spring.p21suck2jo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P21suck2joApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P21suck2joApplication.class, args);
+	}
+
+}
