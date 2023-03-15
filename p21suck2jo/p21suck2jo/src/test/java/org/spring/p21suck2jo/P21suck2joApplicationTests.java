c050688c1983972dc1945504c2b16462d437b9ec
ckdtls1124 <ckdtls1124@naver.com>
2023-03-15 13:59:17 +0900

    0315_defalutProjectFileUpload

---
+++ p21suck2jo/p21suck2jo/src/test/java/org/spring/p21suck2jo/P21suck2joApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.p21suck2jo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P21suck2joApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
