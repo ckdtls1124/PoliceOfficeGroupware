a018ced5bc69c03acd702d843658d20688f93d00
wkdgytjs <gytjs513563@gmail.com>
2023-03-21 17:46:57 +0900

    board,reply entity

--- p21suck2jo/p21suck2jo/src/main/java/org/spring/p21suck2jo/entity/BoardEntity.java
+++ p21suck2jo/p21suck2jo/src/main/java/org/spring/p21suck2jo/entity/BoardEntity.java
@@ -1,9 +1,12 @@
 package org.spring.p21suck2jo.entity;
 
 import lombok.*;
+import org.spring.p21suck2jo.dto.BoardDto;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -34,11 +37,37 @@ public class BoardEntity extends BaseEntity{
     @Column(nullable = false)
     private String boardWriter;
 
-    @ManyToOne
+/*    @ManyToOne
     @JoinColumn(name = "police_id")
-    private PoliceEntity police;
+    private PoliceEntity policeEntity;*/
+
+    @OneToMany(mappedBy = "boardEntity",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<ReplyEntity> replyList = new ArrayList<>();
 
 
+    public static BoardEntity toboardEntity(BoardDto boardDto){
+
+        BoardEntity boardEntity=new BoardEntity();
+
+        boardEntity.setBoardTitle(boardDto.getBoardTitle());
+        boardEntity.setBoardContent(boardDto.getBoardContent());
+        boardEntity.setBoardWriter(boardDto.getBoardWriter());
+        boardEntity.setViews(boardDto.getViews());
+
+        return boardEntity;
+    }
+    public static BoardEntity toAllboardEntity(BoardDto boardDto){
+
+        BoardEntity boardEntity=new BoardEntity();
+
+        boardEntity.setBoardId(boardDto.getBoardId());
+        boardEntity.setBoardTitle(boardDto.getBoardTitle());
+        boardEntity.setBoardContent(boardDto.getBoardContent());
+        boardEntity.setBoardWriter(boardDto.getBoardWriter());
+        boardEntity.setViews(boardDto.getViews());
+
+        return boardEntity;
+    }
 
 
 }
