675b262e823466a991f4073a8b93341a25e8fec9
wkdgytjs <gytjs513563@gmail.com>
2023-03-24 17:54:09 +0900

    reply write, update cancel button addition

--- p21suck2jo/src/main/resources/static/js/board/boardDetail.js
+++ p21suck2jo/src/main/resources/static/js/board/boardDetail.js
@@ -1,18 +1,24 @@
 const writeBtn = document.querySelector(".write-btn");
 const replyWrite = document.querySelector(".reply-write");
+const writeCancelBtn = document.querySelector(".writeCancel");
 const boardHide = "boardHide";
 
 function showReplyWrite(e) {
 	e.preventDefault();
 
-	replyWrite.classList.toggle("boardHide");
+	replyWrite.classList.remove("boardHide");
 }
 
 writeBtn.addEventListener("click", showReplyWrite);
 
+function hideReplyWrite() {
+	replyWrite.classList.add("boardHide");
+}
+
+writeCancelBtn.addEventListener("click", hideReplyWrite);
+
 //const updateBtn=document.querySelector(".update-btn");
 //const replyUpdate=document.querySelector(".reply-update");
-//const updateHide="updateHide";
 //
 //function showReplyUpdate(e){
 //    e.preventDefault();
@@ -23,19 +29,26 @@ writeBtn.addEventListener("click", showReplyWrite);
 //
 //updateBtn.addEventListener("click",showReplyUpdate);
 
-const updateBtns=document.querySelectorAll(".update-btn");
-const replyUpdates=document.querySelector(".reply-update");
+const updateBtns = document.querySelectorAll(".update-btn");
+const replyUpdates = document.querySelector(".reply-update");
+const updateCancelBtn = document.querySelectorAll(".updateCancel");
+const updateHide = "updateHide";
 
-
-updateBtns.forEach((el,idx)=>{
-  el.addEventListener("click",showReplyUpdate2);
+updateBtns.forEach((el, idx) => {
+	el.addEventListener("click", showReplyUpdate);
 });
 
-function showReplyUpdate2(e){
-    e.preventDefault();
-    e.target.nextElementSibling.classList.toggle("updateHide");
-//    replyUpdate.classList.toggle("updateHide");
+function showReplyUpdate(e) {
+	e.preventDefault();
+	e.target.nextElementSibling.classList.remove("updateHide");
+	//    replyUpdate.classList.toggle("updateHide");
 }
 
+function hideReplyUpdate(e) {
+	e.target.nextElementSibling.classList.add("updateHide");
+	//    replyUpdate.classList.toggle("updateHide");
+}
 
-
+updateCancelBtn.forEach((el, idx) => {
+	el.addEventListener("click", hideReplyUpdate);
+});
