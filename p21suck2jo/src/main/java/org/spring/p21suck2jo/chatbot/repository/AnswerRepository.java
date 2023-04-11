ef9cec3e41f3ec6bc0425b0e6ca774f29c436b6f
Devinky <devinky01@gmail.com>
2023-04-11 15:29:25 +0900

    0411

--- p21suck2jo/src/main/java/org/spring/p21suck2jo/chatbot/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package org.spring.chatbot.repository;
-
-import org.spring.chatbot.entity.AnswerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
-}
--- p21suck2jo/src/main/java/org/spring/p21suck2jo/chatbot/repository/ChatDeptRepository.java
+++
@@ -1,7 +0,0 @@
-package org.spring.p21suck2jo.chatbot.repository;
-
-import org.spring.p21suck2jo.chatbot.entity.DeptEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeptRepository extends JpaRepository<DeptEntity, Long> {
-}
