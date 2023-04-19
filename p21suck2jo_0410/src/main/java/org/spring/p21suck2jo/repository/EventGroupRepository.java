f25e51eb81e6e24524cc8ae38ad5534072e3eeec
ckdtls1124 <116870719+ckdtls1124@users.noreply.github.com>
2023-04-19 09:58:41 +0900

    Delete p21suck2jo_0410 directory
    change this file to 'p21suck2jo_ckdtls1124"

--- p21suck2jo_0410/src/main/java/org/spring/p21suck2jo/repository/EventGroupRepository.java
+++
@@ -1,7 +0,0 @@
-package org.spring.p21suck2jo.repository;
-
-import org.spring.p21suck2jo.entity.EventGroupEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventGroupRepository extends JpaRepository<EventGroupEntity, Long> {
-}
--- p21suck2jo_0410/src/main/java/org/spring/p21suck2jo/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package org.spring.p21suck2jo.repository;
-
-import org.spring.p21suck2jo.entity.PersonEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
-}
