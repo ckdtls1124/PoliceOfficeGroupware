fa0d2052ad3a52ba879ab3034c14ac09a946a258
hyoseonJang <116870668+wkdgytjs@users.noreply.github.com>
2023-04-24 16:37:48 +0900

    gytjs-refactory

--- p21suck2jo_refactored/src/main/java/org/spring/p21suck2jo/controller/BoardController.java
+++ p21suck2jo_refactored/src/main/java/org/spring/p21suck2jo/controller/BoardController.java
@@ -1,7 +1,6 @@
 package org.spring.p21suck2jo.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.spring.p21suck2jo.config.UserDetailSecurity;
 import org.spring.p21suck2jo.dto.BoardDto;
 import org.spring.p21suck2jo.dto.PoliceDto;
 import org.spring.p21suck2jo.dto.ReplyDto;
@@ -12,23 +11,13 @@ import org.spring.p21suck2jo.service.ReplyService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -39,24 +28,26 @@ public class BoardController {
     private final ReplyService replyService;
     private final PoliceService policeService;
 
+    // 게시글 등록 View
     @GetMapping("/boardWrite")
-    public String boardWriteView(@AuthenticationPrincipal UserDetails user, Model model ){
+    public String boardWriteView(@AuthenticationPrincipal UserDetails user, Model model) {
 
-
-        PoliceDto policeDto= policeService.policeEmailSearch(user.getUsername());
-
-        if(policeDto!=null){
-            model.addAttribute("policeName",policeDto.getPoliceName());
+        // 게시글 작성시 @AuthenticationPrincipal을 이용해 로그인 된 사용자 이름 가져오기
+        PoliceDto policeDto = policeService.policeEmailSearch(user.getUsername());
+        // 로그인 사용자가 있으면 model로 넘겨주기
+        if (policeDto != null) {
+            model.addAttribute("policeName", policeDto.getPoliceName());
         }
 
         model.addAttribute("boardDto", new BoardDto());
         return "/board/boardWrite";
     }
 
+    // 게시글 등록 Form
     @PostMapping("/boardWrite")
-    public String boardWrite(BoardDto boardDto, Model model){
+    public String boardWrite(BoardDto boardDto) {
 
-        if(boardDto==null){
+        if (boardDto == null) {
             return "/board/boardWrite";
         }
         boardService.boardWrite(boardDto);
@@ -64,63 +55,41 @@ public class BoardController {
         return "redirect:/board";
     }
 
+    // 게시글 목록
     @GetMapping("/board")
-    public String boardList(Model model, @PageableDefault(page=0,size=15,sort="boardId",
-            direction = Sort.Direction.DESC)Pageable pageable,
-                            @RequestParam(value="search",required = false)String search){
+    public String boardList(Model model, @PageableDefault(page = 0, size = 15, sort = "boardId",
+            direction = Sort.Direction.DESC) Pageable pageable,
+                            @RequestParam(value = "search", required = false) String search) {
 
-        if(search==null){
-            Page<BoardDto> boardDtos=boardService.boarListPaging(pageable);
+        // 검색어가 없으면 게시글 목록 paging
+        Page<BoardDto> listPaging;
+        listPaging = boardService.boarListPaging(pageable);
 
-            Long total=boardDtos.getTotalElements();
-            int bockNum=4;
-            int nowPage= boardDtos.getNumber()+1 ;
-            int startPage=Math.max(1,boardDtos.getNumber()-bockNum);
-            int endPage=boardDtos.getTotalPages();
-
-            model.addAttribute("boardDtos",boardDtos);
-            model.addAttribute("total",total);
-            model.addAttribute("nowPage",nowPage);
-            model.addAttribute("startPage",startPage);
-            model.addAttribute("endPage",endPage);
-        }else{
-            Page<BoardDto> boardDtos=boardService.boarListSearchPaging(search,pageable);
-
-            Long total=boardDtos.getTotalElements();
-            int bockNum=4;
-            int nowPage= boardDtos.getNumber()+1 ;
-            int startPage=Math.max(1,boardDtos.getNumber()-bockNum);
-            int endPage=boardDtos.getTotalPages();
-
-            model.addAttribute("boardDtos",boardDtos);
-            model.addAttribute("total",total);
-            model.addAttribute("nowPage",nowPage);
-            model.addAttribute("startPage",startPage);
-            model.addAttribute("endPage",endPage);
+        // 검색어가 있으면 검색된 목록 paging
+        if (search != null) {
+            listPaging = boardService.boarListSearchPaging(search, pageable);
         }
+
+        Long total = listPaging.getTotalElements();// 전체 레코드 수
+        int bockNum = 4;
+        int nowPage = listPaging.getNumber() + 1;// 현재페이지 -> boardDtos.getNumber()는 0부터 시작
+        int startPage = Math.max(1, listPaging.getNumber() - bockNum);// 시작페이지 -> 기본이 최소 1페이지
+        int endPage = listPaging.getTotalPages();// 마지막페이지
+
+        model.addAttribute("boardDtos", listPaging);
+        model.addAttribute("total", total);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
         return "/board/board";
     }
 
+    // 게시글 상세 목록
     @GetMapping("/boardDetail/{boardId}/{key}")
-    public String boardDetail(@PathVariable("boardId") Long boardId,@AuthenticationPrincipal UserDetails user,
+    public String boardDetail(@PathVariable("boardId") Long boardId, @AuthenticationPrincipal UserDetails user,
                               @PathVariable(value = "key", required = false) String key,
-                              Model model){
-
-        // key값이 true이거나 null이 아닌경우 조회수 카운팅 X
-        if(key.equals("true") && key!=null) {
-
-            boardService.upViews2(boardId);
-        // key값이 true가 아니거나 null인 경우 조회수 카운팅 O
-        }else{
-            boardService.upViews(boardId);
-        }
-        // 해당 게시판 번호를 받아 리뷰 상세페이지로 넘겨줌
-        BoardDto boardDtos= boardService.boardDetail(boardId);
-
-        PoliceDto policeName= policeService.policeEmailSearch(user.getUsername());
-
-        model.addAttribute("policeReplyName",policeName.getPoliceName());
-        model.addAttribute("boardDtos",boardDtos);
+                              Model model) {
 
 //        Cookie[] cookies= request.getCookies();
 //
@@ -135,10 +104,27 @@ public class BoardController {
 //                }
 //            }
 //        }
-        if(boardDtos!=null){
-            model.addAttribute("boardDtos",boardDtos);
-//
-//            // 만일 oldCookie가 null이 아니고 oldCookie값에 id값이 없을 때 (있다면 이미 조회한 게시물로 조회수가 올라가지 않음) 조회수 올리는 메소드 호출
+
+        // key값이 true이거나 null이 아닌경우 조회수 카운팅 X
+        if (key.equals("true") && key != null) {
+            boardService.noUpViews(boardId);
+
+            // key값이 true가 아니거나 null인 경우 조회수 카운팅 O
+        } else {
+            // 게시글 조회수 1증가
+            boardService.upViews(boardId);
+        }
+
+        // 해당 게시판의 번호를 받아 게시글 상세페이지로 넘겨줌
+        BoardDto boardDtos = boardService.boardDetailList(boardId);
+
+
+        if (boardDtos != null) {
+
+            model.addAttribute("boardDtos", boardDtos);
+
+            //만일 oldCookie가 null이 아니고 oldCookie값에 id값이 없을 때
+            // (있다면 이미 조회한 게시물로 조회수가 올라가지 않음) 조회수 올리는 메소드 호출
 //            if (oldCookie!=null) {
 //
 //                if(!oldCookie.getValue().contains("["+ boardId.toString() +"]")){
@@ -148,43 +134,53 @@ public class BoardController {
 //                    //쿠기를 추가 시키고 조회수 증가시킴
 //                    boardService.upViews(boardId);
 //                }
-//                // oldCookie가 null일 경우 postView라는 이름으로 쿠키를 만들고 조회수 올리는 메소드 호출
+//            // oldCookie가 null일 경우 postView라는 이름으로 쿠키를 만들고 조회수 올리는 메소드 호출
 //            }else{
 //                Cookie newCookie = new Cookie("postView", "[" + boardId + "]");
 //                newCookie.setMaxAge(-1);
 //                response.addCookie(newCookie);
 //
 //                boardService.upViews(boardId);
-//            }
-            List<ReplyDto> replyList=replyService.replyList(boardId);
-            model.addAttribute("replyList",replyList);
+
+
+            // 댓글 작성시 로그인 된 사용자 이름 가져오기
+            PoliceDto policeName = policeService.policeEmailSearch(user.getUsername());
+            model.addAttribute("policeReplyName", policeName.getPoliceName());
+
+            // 댓글 목록
+            List<ReplyDto> replyList = replyService.replyList(boardId);
+            model.addAttribute("replyList", replyList);
+
             return "/board/boardDetail";
-        }else{
+        } else {
             return null;
         }
     }
 
+    // 게시글 수정 View
     @GetMapping("/boardUpdate/{boardId}")
-    public String boardUpdate(@PathVariable("boardId") Long boardId, Model model){
+    public String boardUpdate(@PathVariable("boardId") Long boardId, Model model) {
 
-        BoardDto boardDto=boardService.boardUpdate(boardId);
-
-        model.addAttribute("boardDto",boardDto);
+        // 해당 게시판의 번호를 받아 게시글 수정페이지로 넘겨줌
+        BoardDto boardDto = boardService.boardUpdate(boardId);
+        model.addAttribute("boardDto", boardDto);
 
         return "board/boardUpdate";
     }
 
+    // 게시글 수정 Form
     @PostMapping("/boardUpdate")
     public String boardUpdateOk(@ModelAttribute BoardDto boardDto,
-                                @RequestParam(value = "key",required = false) String key){
+                                @RequestParam(value = "key", required = false) String key) {
 
         boardService.boardUpdateOk(boardDto);
 
-        return "redirect:/boardDetail/"+boardDto.getBoardId()+"/"+key;
+        return "redirect:/boardDetail/" + boardDto.getBoardId() + "/" + key;
     }
 
+    // 게시글 삭제
     @GetMapping("/boardDelete/{boardId}")
-    public String boardDelete(@PathVariable("boardId") Long boardId){
+    public String boardDelete(@PathVariable("boardId") Long boardId) {
 
         boardService.boardDelete(boardId);
 
--- p21suck2jo_refactored/src/main/java/org/spring/p21suck2jo/controller/ReplyController.java
+++ p21suck2jo_refactored/src/main/java/org/spring/p21suck2jo/controller/ReplyController.java
@@ -1,13 +1,8 @@
 package org.spring.p21suck2jo.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.spring.p21suck2jo.dto.BoardDto;
-import org.spring.p21suck2jo.dto.PoliceDto;
 import org.spring.p21suck2jo.dto.ReplyDto;
-import org.spring.p21suck2jo.service.PoliceService;
 import org.spring.p21suck2jo.service.ReplyService;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,44 +14,47 @@ import java.util.List;
 public class ReplyController {
 
     private final ReplyService replyService;
-    private final PoliceService policeService;
 
+    // 댓글 작성 및 목록
     @PostMapping("/replyWrite")
     public String replyWrite(@ModelAttribute ReplyDto replyDto, Model model,
-                             @RequestParam(value = "key",required = false) String key){
+                             @RequestParam(value = "key", required = false) String key) {
 
+        // 댓글 작성
+        replyService.replyWrite(replyDto);
+//      System.out.println("id >>>"+replyDto.getBoardId());
 
-        replyService.insertReply(replyDto);
+        // 댓글 목록 -> 게시글 id(댓글을 단 게시글의 id)의 리스트만 get
+        // 댓글을 작성하고 -> 게시글이 존재하는지 확인 -> DTO-> Entity -> 저장
+        // 상세목록페이지에 글번호와 댓글리스트를 가지고 같이 보낸다.
+        List<ReplyDto> replyList = replyService.replyList(replyDto.getBoardId());
+        model.addAttribute("replyList", replyList);
 
-        List<ReplyDto> replyList= replyService.replyList(replyDto.getBoardId());
-
-        model.addAttribute("replyList",replyList);
-
-        return "redirect:/boardDetail/"+ replyDto.getBoardId()+"/"+key;
+        //게시글 id -> replyDto.getBoardId()/+ 조회수 key
+        return "redirect:/boardDetail/" + replyDto.getBoardId() + "/" + key;
     }
 
+    // 댓글 수정
     @PostMapping("/replyUpdate")
     public String replyUpdate(@ModelAttribute ReplyDto replyDto, Model model,
-                              @RequestParam(value = "key",required = false) String key){
+                              @RequestParam(value = "key", required = false) String key) {
+        Long updateReply = replyService.replyUpdate(replyDto);
 
-        Long updateReply=replyService.updateReply(replyDto);
+        model.addAttribute("updateReply", updateReply);
 
-        model.addAttribute("updateReply",updateReply);
-
-        return "redirect:/boardDetail/"+ replyDto.getBoardId()+"/"+key;
+        return "redirect:/boardDetail/" + replyDto.getBoardId() + "/" + key;
     }
 
+    // 댓글 삭제
     @GetMapping("/replyDelete")
-    public String replyDelete(@RequestParam Long boardId, @RequestParam Long replyId, @RequestParam String key){
+    public String replyDelete(@RequestParam Long boardId, @RequestParam Long replyId,
+                              @RequestParam(value = "key", required = false) String key) {
 
-        replyService.replyDelete(replyId,boardId);
+        // replyId와 boardId를 받아서 테이블에 존재하는 해당 컬럼만 삭제 한다.
+        replyService.replyDelete(replyId, boardId);
 
-        return "redirect:/boardDetail/"+boardId+"/"+key;
+        return "redirect:/boardDetail/" + boardId + "/" + key;
     }
-
-
-
-
 
 
 }
